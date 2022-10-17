2134e0f79b5077e7566d733ec015a1048f69fdc2
sujon-chambugong <sujonchambugong@gmail.com>
2022-10-17 23:38:00 +0600

    header, footer, layout, banner, latest post created

--- components/HomeSections/LatestPost.js
+++ components/HomeSections/LatestPost.js
@@ -4,6 +4,12 @@ const LatestPost = () => {
     return (
         <div>
             <h1>Latest post</h1>
+            <div>
+                <div>
+                    <h1>This is my first post</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore illum cupiditate officiis dolor porro dolorem voluptatibus quia omnis enim id commodi quae nemo est, quis excepturi, veniam quidem delectus quasi.</p>
+                </div>
+            </div>
         </div>
     );
 };
